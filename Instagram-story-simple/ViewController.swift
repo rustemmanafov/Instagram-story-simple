2428ca51c5ebca3a7fd69ca853882fa620822cb2
Rustam Manafli <rustemmanafov1@gmail.com>
2023-01-03 20:38:08 +0400

    Created

---
+++ Instagram-story-simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram-story-simple
+//
+//  Created by Rustem Manafov on 03.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
